f36b2fc0d2fdaf601272bb42c4bc7b1ba6154af3
지농 <jinhong0719@naver.com>
2021-06-16 23:46:10 +0900

    feat(users): create Students model & add field about account value

--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.core.validators import RegexValidator
+from postgres_composite_types import CompositeType
 
-# Create your models here.
+
+class Students(models.Model):
+    """Students model having account value(uuid, id, pw) and student inform(number, phone, etc ...)"""
+
+    # primary key of Students model
+    # postgresql -> "uuid" varchar(20) NOT NULL PRIMARY KEY)
+    uuid = models.CharField(primary_key=True, max_length=20, validators=[
+        RegexValidator(regex='^student-\\d{12}'),  # check uuid value with '^student-\d{12}' regex
+    ])
+
+    # id value of student account
+    student_id = models.CharField(max_length=20, unique=True)
+
+    # hashed password value of student account
+    student_pw = models.TextField()
