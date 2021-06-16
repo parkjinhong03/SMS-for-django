8a044f5b7a3e0ccd63ac5b1fab73f1355d488260
지농 <jinhong0719@naver.com>
2021-06-16 23:55:11 +0900

    refactor(settings): change to make to get secret value with env

--- app/settings.py
+++ app/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import environ
+
+# environ.Env.read_env() -> set .env file
+env = environ.Env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pvcob_+91vhq-xl2qy1+s@$@j0rp@ika)z-llq5^7z3*gdf$^%'
+SECRET_KEY = env('SMS_FOR_DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
