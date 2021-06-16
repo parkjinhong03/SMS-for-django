d9defaeda1cff17a6fb61ea441f724c1288ca486
지농 <jinhong0719@naver.com>
2021-06-16 23:46:51 +0900

    feat(users): add StudentNumber field which is composite type in Students

--- users/models.py
+++ users/models.py
@@ -17,3 +17,20 @@ class Students(models.Model):
 
     # hashed password value of student account
     student_pw = models.TextField()
+
+    class StudentNumber(CompositeType):
+        """composite type for postgresql field having grade, group, number value"""
+        grade = models.SmallIntegerField()   # grade(학년) value of student number type
+        group = models.SmallIntegerField()   # group(반) value of student number type
+        number = models.SmallIntegerField()  # number(번호) value of student number type
+
+        class Meta:
+            db_type = 'student_number'
+
+    # student number value contains grade, group, number of student
+    student_number = StudentNumber.Field()
+
+    # def save(self, *args, **kwargs):
+    #     student_number = self.student_number
+    #     self.student_number = (student_number['grade'], student_number['group'], student_number['number'])
+    #     return super(Students, self).save(*args, **kwargs)
