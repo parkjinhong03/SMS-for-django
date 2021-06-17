f932c5a1bfbc17d62c7e34f55d5cb8f2e92e66b3
지농 <jinhong0719@naver.com>
2021-06-17 10:10:48 +0900

    feat(users): add field about student inform & move StudentNumber class to outer

--- users/models.py
+++ users/models.py
@@ -1,6 +1,26 @@
 from django.db import models
-from django.core.validators import RegexValidator
+from django.core import validators
 from postgres_composite_types import CompositeType
+
+
+class StudentNumber(CompositeType):
+    """custom composite type for postgresql field having grade, group, number value"""
+    # grade(학년) value of student number type
+    grade = models.SmallIntegerField(null=True, validators=[
+        validators.MinValueValidator(limit_value=1), validators.MaxValueValidator(limit_value=3),
+    ])
+    # group(반) value of student number type
+    group = models.SmallIntegerField(null=True, validators=[
+        validators.MinValueValidator(limit_value=1), validators.MaxValueValidator(limit_value=4),
+    ])
+    # number(번호) value of student number type
+    number = models.SmallIntegerField(null=True, validators=[
+        validators.MinValueValidator(limit_value=1), validators.MaxValueValidator(limit_value=21),
+    ])
+
+    class Meta:
+        # set type name of StudentNumber in postgresql
+        db_type = 'student_number'
 
 
 class Students(models.Model):
@@ -9,7 +29,7 @@ class Students(models.Model):
     # primary key of Students model
     # postgresql -> "uuid" varchar(20) NOT NULL PRIMARY KEY)
     uuid = models.CharField(primary_key=True, max_length=20, validators=[
-        RegexValidator(regex='^student-\\d{12}'),  # check uuid value with '^student-\d{12}' regex
+        validators.RegexValidator(regex='^student-\\d{12}$'),  # check uuid value with regular expression
     ])
 
     # id value of student account
@@ -18,19 +38,16 @@ class Students(models.Model):
     # hashed password value of student account
     student_pw = models.TextField()
 
-    class StudentNumber(CompositeType):
-        """composite type for postgresql field having grade, group, number value"""
-        grade = models.SmallIntegerField()   # grade(학년) value of student number type
-        group = models.SmallIntegerField()   # group(반) value of student number type
-        number = models.SmallIntegerField()  # number(번호) value of student number type
-
-        class Meta:
-            db_type = 'student_number'
-
     # student number value contains grade, group, number of student
-    student_number = StudentNumber.Field()
+    student_number = StudentNumber.Field(blank=True)
 
-    # def save(self, *args, **kwargs):
-    #     student_number = self.student_number
-    #     self.student_number = (student_number['grade'], student_number['group'], student_number['number'])
-    #     return super(Students, self).save(*args, **kwargs)
+    # name value of student inform
+    name = models.CharField(max_length=10)
+
+    # phone number value of student inform
+    phone_number = models.CharField(max_length=11, unique=True, validators=[
+        validators.RegexValidator(regex='^\\d{3}\\d{3,4}\\d{4}$')  # check phone number value with regular expression
+    ])
+
+    # path value in profile uri of student profile
+    profile_uri_path = models.TextField(unique=True)
