e1b5848dfa4f3d57b4e8bb4d9d8b4415cfa807b2
지농 <jinhong0719@naver.com>
2021-06-17 10:18:31 +0900

    docs(users): add comment about postgreSQL in model

--- users/models.py
+++ users/models.py
@@ -4,18 +4,23 @@ from postgres_composite_types import CompositeType
 
 
 class StudentNumber(CompositeType):
-    """custom composite type for postgresql field having grade, group, number value"""
-    # grade(학년) value of student number type
+    """custom composite type for postgresql field having grade, group, number value
+
+    (SQL in postgresql)
+    CREATE TYPE "student_number" AS ("grade" smallint, "group" smallint, "number" smallint);
+    """
+
+    # grade(학년) value of student number type (postgresql -> "grade" smallint)
     grade = models.SmallIntegerField(null=True, validators=[
-        validators.MinValueValidator(limit_value=1), validators.MaxValueValidator(limit_value=3),
+        validators.MinValueValidator(limit_value=1), validators.MaxValueValidator(limit_value=3),  # 1~3 between value
     ])
-    # group(반) value of student number type
+    # group(반) value of student number type (postgresql -> "group" smallint)
     group = models.SmallIntegerField(null=True, validators=[
-        validators.MinValueValidator(limit_value=1), validators.MaxValueValidator(limit_value=4),
+        validators.MinValueValidator(limit_value=1), validators.MaxValueValidator(limit_value=4),  # 1~4 between value
     ])
-    # number(번호) value of student number type
+    # number(번호) value of student number type (postgresql -> "number" smallint)
     number = models.SmallIntegerField(null=True, validators=[
-        validators.MinValueValidator(limit_value=1), validators.MaxValueValidator(limit_value=21),
+        validators.MinValueValidator(limit_value=1), validators.MaxValueValidator(limit_value=21),  # 1~21 between value
     ])
 
     class Meta:
@@ -24,32 +29,43 @@ class StudentNumber(CompositeType):
 
 
 class Students(models.Model):
-    """Students model having account value(uuid, id, pw) and student inform(number, phone, etc ...)"""
+    """Students model having account value(uuid, id, pw) and student inform(number, phone, etc ...)
 
-    # primary key of Students model
-    # postgresql -> "uuid" varchar(20) NOT NULL PRIMARY KEY)
+    (SQL in postgresql)
+    CREATE TABLE "users_students" (
+        "uuid"             varchar(20) NOT NULL PRIMARY KEY,
+        "student_id"       varchar(20) NOT NULL UNIQUE,
+        "student_pw"       text NOT NULL,
+        "student_number"   student_number NULL,
+        "name"             varchar(10) NOT NULL,
+        "phone_number"     varchar(11) NOT NULL UNIQUE,
+        "profile_uri_path" text NOT NULL UNIQUE
+    );
+    """
+
+    # primary key of Students model (postgresql -> "uuid" varchar(20) NOT NULL PRIMARY KEY))
     uuid = models.CharField(primary_key=True, max_length=20, validators=[
         validators.RegexValidator(regex='^student-\\d{12}$'),  # check uuid value with regular expression
     ])
 
-    # id value of student account
+    # id value of student account (postgresql -> varchar(20) NOT NULL UNIQUE)
     student_id = models.CharField(max_length=20, unique=True)
 
-    # hashed password value of student account
+    # hashed password value of student account (postgresql -> text NOT NULL)
     student_pw = models.TextField()
 
-    # student number value contains grade, group, number of student
+    # student number value contains grade, group, number of student (postgresql -> student_number NULL)
     student_number = StudentNumber.Field(blank=True)
 
-    # name value of student inform
+    # name value of student inform (postgresql -> varchar(10) NOT NULL)
     name = models.CharField(max_length=10)
 
-    # phone number value of student inform
+    # phone number value of student inform (postgresql -> varchar(11) NOT NULL UNIQUE)
     phone_number = models.CharField(max_length=11, unique=True, validators=[
         validators.RegexValidator(regex='^\\d{3}\\d{3,4}\\d{4}$')  # check phone number value with regular expression
     ])
 
-    # path value in profile uri of student profile
+    # path value in profile uri of student profile (postgresql -> text NOT NULL UNIQUE)
     profile_uri_path = models.TextField(unique=True)
 
     def save(self, *args, **kwargs):
