63d4154643b80054959f43e5f8eb148c5a2024bc
지농 <jinhong0719@naver.com>
2021-06-17 20:11:22 +0900

    feat(root.urls): add users URLconf to root URLconf with include

--- app/urls.py
+++ app/urls.py
@@ -13,10 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.urlpatterns import format_suffix_patterns
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns, suffix_required=True, allowed=['json', 'html'])
