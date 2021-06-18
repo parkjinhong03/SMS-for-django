faa6a4d8cbbeabe5ae76296ed621f54a65c5c469
지농 <jinhong0719@naver.com>
2021-06-18 16:32:19 +0900

    refactor(app.responses): change Response __init__ method type hint

--- app/responses.py
+++ app/responses.py
@@ -6,7 +6,7 @@ from rest_framework.status import HTTP_200_OK
 class Response(Response):
     """add status, code, msg values which are received from kwarg to data dict"""
 
-    def __init__(self, data: any = None, status: int = HTTP_200_OK, code: int = 0, msg: str = '', *args, **kwargs):
+    def __init__(self, data: any = None, status: int = HTTP_200_OK, code: int = 0, msg: any = '', *args, **kwargs):
         data = {} if data is None else data
         if isinstance(data, abc.MutableMapping):
             data['status'] = status
