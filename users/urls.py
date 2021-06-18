6c610aebfff7b5cd6c389784a2a2882cde1442d7
지농 <jinhong0719@naver.com>
2021-06-18 16:26:56 +0900

    feat(users): add hashing_codec dependency in StudentBasicSignup view

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from hash import bcrypt
 
 app_name = 'users'
 urlpatterns = [
-    path('students', views.StudentBasicSignup.as_view(), name='student-basic-sign-up'),
+    path('students', views.StudentBasicSignup.as_view(
+        hashing_codec=bcrypt.BcryptHashingCodec(),
+    ), name='student-basic-sign-up'),
 ]
