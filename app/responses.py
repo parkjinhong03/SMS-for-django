6864d6221a07a86c414e2f58775d7dede5f2df45
지농 <jinhong0719@naver.com>
2021-06-20 21:25:12 +0900

    refactor(responses): change how to set response data in Response

--- app/responses.py
+++ app/responses.py
@@ -6,10 +6,8 @@ from rest_framework.status import HTTP_200_OK
 class Response(Response):
     """add status, code, msg values which are received from kwarg to data dict"""
 
-    def __init__(self, data: any = None, status: int = HTTP_200_OK, code: int = 0, msg: any = '', *args, **kwargs):
+    def __init__(self, status: int = HTTP_200_OK, code: int = 0, msg: any = '', data: any = None, *args, **kwargs):
         data = {} if data is None else data
         if isinstance(data, abc.MutableMapping):
-            data['status'] = status
-            data['code'] = code
-            data['message'] = msg
-        super(Response, self).__init__(data=data, status=status, *args, **kwargs)
+            data['status'], data['code'], data['message'] = status, code, msg
+        super(Response, self).__init__(status=status, data=data, *args, **kwargs)
