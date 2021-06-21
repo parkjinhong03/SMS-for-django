f5602c0bf1f63f6463cb6c90ea65e8161788d09c
지농 <jinhong0719@naver.com>
2021-06-21 22:38:20 +0900

    feat(users.permissions): add method check if uuid payload token is exist

---
+++ users/permissions.py
@@ -0,0 +1,34 @@
+from jwt.exceptions import PyJWTError
+from django.conf import settings
+from rest_framework import permissions
+from app.exceptions import UnexpectedError, NotAuthenticatedError
+
+
+class IsAuthenticated(permissions.BasePermission):
+    """custom permission that check if token with uuid payload is exist in authorization"""
+    
+    def has_permission(self, request, view):
+        if (jwt_codec := getattr(view, 'jwt_codec', None)) is None:
+            raise UnexpectedError(AttributeError('view doesn\'t have jwt_codec attribute'), detail='in has_permission')
+
+        if (author := request.META.get('HTTP_AUTHORIZATION', None)) is None:
+            raise NotAuthenticatedError('authorization is not exist in request META', code=-11)
+
+        try:
+            _type, token = author.split(' ')  # raise ValueError if invalid format
+            if _type == 'Bearer':
+                payload = jwt_codec.decode(token, key=settings.JWT_SECRET_KEY)  # raise PyJWTError if invalid token
+                _, _ = payload['uuid'], payload['type']  # raise KeyError if not exist
+                request.token_payload = payload
+            else:
+                raise NotImplementedError  # raise NotImplementedError if unsupported token type
+        except ValueError:
+            raise NotAuthenticatedError('invalid format of authorization', code=-12)
+        except NotImplementedError:
+            raise NotAuthenticatedError('unsupported token type', code=-13)
+        except PyJWTError as e:
+            raise NotAuthenticatedError(f'invalid token in authorization, err: {e}', code=-14)
+        except KeyError:
+            raise NotAuthenticatedError('invalid payload value in token', code=-15)
+
+        return True
