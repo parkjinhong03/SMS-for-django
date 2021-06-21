427cd74807186354c1ea9b7dad93291ef3b4fc7c
지농 <jinhong0719@naver.com>
2021-06-21 11:52:13 +0900

    feat(storage.s3): add S3Storage & add method that put object to s3

---
+++ storage/s3.py
@@ -0,0 +1,12 @@
+import boto3
+from django.conf import settings
+
+
+class S3Storage:
+    """object storage using AWS S3"""
+
+    def __init__(self, access_id: str = settings.AWS_S3_ID, access_key: str = settings.AWS_S3_KEY):
+        self.client = boto3.client('s3', aws_access_key_id=access_id, aws_secret_access_key=access_key)
+
+    def put_object(self, body, key, bucket=settings.AWS_S3_BUCKET, acl='public-read') -> None:
+        self.client.put_object(ACL=acl, Body=body, Bucket=bucket, Key=key)
