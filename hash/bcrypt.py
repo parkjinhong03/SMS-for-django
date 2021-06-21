85fbc9b490e25c05ee5f8dffafec90e619fb5a5d
지농 <jinhong0719@naver.com>
2021-06-21 11:25:10 +0900

    refactor(hash): change BcryptHashingCodec method name & add __init__

--- hash/bcrypt.py
+++ hash/bcrypt.py
@@ -3,9 +3,11 @@ import bcrypt
 
 class BcryptHashingCodec:
     """class that hash password & compare with hashed using bcrypt algorithm"""
+    def __init__(self, encoding='utf8'):
+        self.encoding = encoding
 
-    def hashpw(self, password: str, salt: bytes = bcrypt.gensalt(rounds=4)) -> str:
-        return bcrypt.hashpw(bytes(password, encoding='utf8'), salt).decode(encoding='utf8')
+    def encode(self, password: str, salt: bytes = bcrypt.gensalt(rounds=4)) -> str:
+        return bcrypt.hashpw(bytes(password, self.encoding), salt).decode(encoding=self.encoding)
 
-    def checkpw(self, password: str, hashed: str) -> bool:
-        return bcrypt.checkpw(bytes(password, encoding='utf8'), bytes(hashed, encoding='utf8'))
+    def compare_hash(self, password: str, hashed: str) -> bool:
+        return bcrypt.checkpw(bytes(password, encoding=self.encoding), bytes(hashed, encoding=self.encoding))
