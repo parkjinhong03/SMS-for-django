335a9ba10551c931b249a8ba5e4239438604e874
지농 <jinhong0719@naver.com>
2021-06-21 11:54:43 +0900

    feat(users): add injecting object_storage dependency with S3Storage

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 from hash import bcrypt
+from storage import s3
 
 app_name = 'users'
 urlpatterns = [
     path('students', views.StudentBasicSignup.as_view(
         hashing_codec=bcrypt.BcryptHashingCodec(),
+        object_storage=s3.S3Storage(),
     ), name='student-basic-sign-up'),
 ]
