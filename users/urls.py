6858302cad589464d5c1b5213e7cd133c381070b
지농 <jinhong0719@naver.com>
2021-06-21 15:01:57 +0900

    feat(users.urls): add views.StudentBasicLogin to path

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,21 @@
 from django.urls import path
 from . import views
-from hash import bcrypt
+from codec import hash, jwt
 from storage import s3
+
+hashing_codec = hash.BcryptHashingCodec()
+object_storage = s3.S3Storage()
+jwt_codec = jwt.PyJWTCodec()
 
 app_name = 'users'
 urlpatterns = [
     path('students', views.StudentBasicSignup.as_view(
-        hashing_codec=bcrypt.BcryptHashingCodec(),
-        object_storage=s3.S3Storage(),
+        hashing_codec=hashing_codec,
+        object_storage=object_storage,
     ), name='student-basic-sign-up'),
+
+    path('login/students', views.StudentBasicLogin.as_view(
+        hashing_codec=hashing_codec,
+        jwt_codec=jwt_codec,
+    ), name='student-basic-login')
 ]
