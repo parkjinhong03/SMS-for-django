9ab3607901841fd5d620ac6a3d6c3893e87ea41e
지농 <jinhong0719@naver.com>
2021-06-25 11:16:34 +0900

    feat(outings.models): add OUtingCards model & Progresses Enum type

--- outings/models.py
+++ outings/models.py
@@ -1,3 +1,46 @@
+from enum import Enum
 from django.db import models
+from django.core import validators
+from django.contrib.gis.db import models as gis_model
 
-# Create your models here.
+
+class OutingCards(models.Model):
+    """Outings model having about outing card value
+
+    (SQL in postgresql)
+    """
+
+    class Progresses(Enum):
+        """
+        0 → 외출 신청
+        1 → 학부모 승인, -1 → 학부모 거절
+        2 → 선생님 승인, -2 → 선생님 거절
+        3 → 외출 시작
+        4 → 외출 종료
+        5 → 외출 인증 승인
+        """
+
+        APPLIED = 0
+        PARENT_ACCEPTED = 1
+        PARENT_REJECTED = -1
+        TEACHER_ACCEPTED = 2
+        TEACHER_REJECTED = -2
+        STARTED = 3
+        FINISHED = 4
+        CERTIFIED = 5
+
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True)
+    uuid = models.CharField(primary_key=True, max_length=24, validators=[
+        validators.RegexValidator(regex='^outing_card-\\d{12}$')
+    ])
+    student_uuid = models.ForeignKey(to='users.Students', on_delete=models.CASCADE, related_name='apply_outings')
+    progress = models.SmallIntegerField()
+    accepted_teacher = models.ForeignKey(to='users.Teachers', on_delete=models.CASCADE,
+                                         related_name='accept_outings', null=True)
+    is_emergency = models.BooleanField(default=False)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    arrival_time = models.DateTimeField(null=True)
+    reason = models.CharField(max_length=1000)
+    outing_place = gis_model.PointField()
