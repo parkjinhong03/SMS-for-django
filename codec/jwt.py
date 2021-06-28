4f68d06ac2178157f19b760b59c9a865baa549b7
지농 <jinhong0719@naver.com>
2021-06-28 11:34:34 +0900

    feat(codec.jwt): add expired_at argument in encode method

--- codec/jwt.py
+++ codec/jwt.py
@@ -1,3 +1,4 @@
+import datetime
 import jwt
 from typing import Tuple, Dict, Any, List
 from _meta.singleton import Singleton
@@ -9,9 +10,13 @@ class PyJWTCodec(metaclass=Singleton):
     def __init__(self, encode_algorithm: str = 'HS256', decode_algorithms: Tuple[str] = ('HS256',)):
         self.encode_algorithm, self.decode_algorithms = encode_algorithm, decode_algorithms
 
-    def encode(self, payload: Dict[str, Any], key: str, encode_algorithm: str = None):
+    def encode(self, payload: Dict[str, Any], key: str, expired_at: datetime.datetime, encode_algorithm: str = None):
         if encode_algorithm is None:
             encode_algorithm = self.encode_algorithm
+        payload = {
+            'exp': expired_at,
+            **payload,
+        }
         return jwt.encode(payload=payload, key=key, algorithm=encode_algorithm)
 
     def decode(self, _jwt: str, key: str, decode_algorithm: List[str] = None):
