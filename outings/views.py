555f284ea0abd18a33291430a127ebe910f07382
지농 <jinhong0719@naver.com>
2021-06-28 15:20:06 +0900

    feat(outing.views): implemenet OutingApplyFromStudent.POST logic

--- outings/views.py
+++ outings/views.py
@@ -1,9 +1,17 @@
+from django.contrib.gis.geos import GEOSGeometry
+from django.db import transaction
 from rest_framework.views import APIView
+
 from app.views import Base
-from app.exceptions import RequestInvalidError
+from app.exceptions import (
+    RequestInvalidError, UnexpectedValidateError, UnexpectedError,
+    contain_code_to_error_string
+)
 from app.responses import Response
 from users import permissions
 from . import requests, interfaces
+from .models import OutingCards
+from .serializers import OutingCardsSerializer
 
 
 class OutingApplyFromStudent(Base,
@@ -20,10 +28,34 @@ class OutingApplyFromStudent(Base,
 
         return super(OutingApplyFromStudent, cls).as_view(**initkwargs)
 
+    @transaction.atomic
     def post(self, request, *args, **kwargs):
         req_serializer = requests.OutingApplyFromStudent.POST(data=request.data)
         if not req_serializer.is_valid():
             raise RequestInvalidError(req_serializer.errors)
-        data = req_serializer.data
 
-        return Response(status=201, msg='succeed to apply new outing from student', data={'request': data})
+        data = req_serializer.validated_data
+        data['uuid'] = OutingCards.get_available_uuid()
+        data['student_uuid'] = request.uuid
+        data['place_point'] = GEOSGeometry(f'POINT({data.pop("place_x")} {data.pop("place_y")})')
+
+        (outing_card_serializer := OutingCardsSerializer(data=data)).is_valid()
+        validate_errors = contain_code_to_error_string(outing_card_serializer.errors)
+
+        if validate_errors:
+            raise UnexpectedValidateError('unexpected fields', validate_errors)
+
+        if OutingCards.objects.filter(start_time__day=data['start_time'].day):
+            return Response(status=407, code=-201, msg='you already apply outing in that day')
+
+        tx = transaction.savepoint()
+        try:
+            outing_card_serializer.save()
+        except Exception as e:
+            transaction.savepoint_rollback(tx)
+            raise UnexpectedError(e, 'unexpected error occurs while saving new outing card')
+        transaction.savepoint_commit(tx)
+
+        return Response(status=201, msg='succeed to apply new outing from student', data={
+            'uuid': outing_card_serializer.validated_data['uuid']
+        })
