4314bfe4830e56aba13a8ca1033aad96c32bb132
지농 <jinhong0719@naver.com>
2021-06-29 16:01:06 +0900

    feat(outings.interfaces): add ElasticsearchAgency interface

--- outings/interfaces.py
+++ outings/interfaces.py
@@ -2,10 +2,19 @@ from abc import ABCMeta
 
 
 class JWTCodec(metaclass=ABCMeta):
-    """interface to jwt_codec dependency using in View initialize"""
+    """interface to jwt_codec dependency used in view"""
 
     @classmethod
     def __subclasshook__(cls, subclass):
         return (hasattr(subclass, 'encode') and callable(subclass.encode) and
                 hasattr(subclass, 'decode') and callable(subclass.decode)) or \
+               NotImplemented
+
+
+class ElasticsearchAgency(metaclass=ABCMeta):
+    """interface to elasticsearch agent dependency used in view"""
+
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return (hasattr(subclass, 'search') and callable(subclass.search)) or \
                NotImplemented
