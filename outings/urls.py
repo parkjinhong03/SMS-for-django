ca3467c48e0e2b10c2947513cdc6f992a54f29fa
지농 <jinhong0719@naver.com>
2021-06-29 16:02:42 +0900

    refactor(outings.urls): change OutingApplyFromStudent view path

--- outings/urls.py
+++ outings/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 from codec.jwt import PyJWTCodec
 
-pyjwt_codec = PyJWTCodec(common=True)
+pyjwt_codec = PyJWTCodec(single=True)
 
 app_name = 'outings'
 urlpatterns = [
-    path('/', views.OutingApplyFromStudent.as_view(
-        jwt_codec=pyjwt_codec
-    ))
+    path('/outing-cards', views.OutingApplyFromStudent.as_view(
+        jwt_codec=pyjwt_codec,
+    )),
 ]
