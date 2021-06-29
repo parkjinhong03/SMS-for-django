2c3de784112b4b8ad42475220fc4267e6c491c34
지농 <jinhong0719@naver.com>
2021-06-29 16:05:45 +0900

    feat(outing.urls): add OutingsSearch view in outings URLConf

--- outings/urls.py
+++ outings/urls.py
@@ -1,12 +1,22 @@
 from django.urls import path
+from django.conf import settings
+from elasticsearch import Elasticsearch
+
 from . import views
 from codec.jwt import PyJWTCodec
+from es.agent import ElasticsearchAgent
 
 pyjwt_codec = PyJWTCodec(single=True)
+elasticsearch_agent = ElasticsearchAgent(single=True, client=Elasticsearch(hosts=settings.ELASTICSEARCH_HOSTS,
+                                                                           port=settings.ELASTICSEARCH_PORT))
 
 app_name = 'outings'
 urlpatterns = [
     path('/outing-cards', views.OutingApplyFromStudent.as_view(
         jwt_codec=pyjwt_codec,
+    )),
+    path('/outing-cards/_search', views.OutingsSearch.as_view(
+        jwt_codec=pyjwt_codec,
+        elasticsearch_agency=elasticsearch_agent,
     )),
 ]
